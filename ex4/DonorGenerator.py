26462b7fb0c97b4a3998343e6ddef4db439da99b
bradeck <bradeck@agile-im.de>
2018-05-23 13:00:34 +0200

    working on donorgenerator

--- ex4/DonorGenerator.py
+++ ex4/DonorGenerator.py
@@ -1,3 +1,5 @@
+import numpy as np
+from PPP import PPP
 
 class DonorGenerator:
     """
@@ -9,7 +11,39 @@ class DonorGenerator:
 
     def __init__(self, F):
         self.F = F
-        pass
 
-    def generate_donor(self):
-        pass
+    def generate_donor(self, x, generation):
+        """
+
+        :param x: an individual from the current generation
+        :param generation: the curent generation
+        :return: a single donor vector for the individual
+        """
+
+        # pic randomly 3 individuals from the current generation:
+        # but not x
+        choice_of_individuals = generation
+        choice_of_individuals.remove(x)
+
+        vectors = np.random.choice(choice_of_individuals, 3)
+        x1 = vectors[0]
+        x2 = vectors[1]
+        x3 = vectors[2]
+
+        diff = np.substract(x2, x3)
+
+        donor = x1 + F*diff
+        return donor
+
+
+
+    def generate_donor_for_pop(self,generation):
+        """
+
+        :param generation: the current generation
+        :return: a list of donors, one donor for each individual from the current generation
+        """
+        donors = []
+        for x in generation:
+            donors.append(self.mutate(x, generation))
+        return donors
