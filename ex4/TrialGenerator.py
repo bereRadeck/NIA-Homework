d6172ecf787cdad8f4607377082acc1a20e7422b
arontaupe <aron@petau.net>
2018-05-23 13:07:47 +0200

    first try

--- ex4/TrialGenerator.py
+++ ex4/TrialGenerator.py
@@ -1,3 +1,7 @@
+import random
+
+import numpy as np
+
 
 class TrialGenerator:
     """
@@ -6,9 +10,21 @@ class TrialGenerator:
     through binomial crossover
     """
 
-    def __init__(self, Cr):
+    def __init__(self, Cr, generation, donor_vector):
         self.Cr = Cr
-        pass
+        self.generation = generation
+        self.donor_vector = donor_vector
 
-    def generate_trial(self):
-        pass
+
+    def generate_trial(self, generation, donor_vector):
+        list_of_trialvectors = []
+        random_index_j = np.randint(0,len(generation))
+
+            #once for every target vector
+        for i in generation:
+            if np.random() <= self.Cr or random_index_j == i: #we take the donor vector
+                list_of_trialvectors.append(donor_vector[i])
+            else: # we just take the old vector
+                list_of_trialvectors.append(generation[i])
+
+    return list_of_trialvectors
