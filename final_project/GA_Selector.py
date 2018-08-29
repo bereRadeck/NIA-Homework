d2cf3cdd0b868a2ea620196abfcaa7a46ca7302d
ljscfo <fbidts@yahoo.de>
2018-08-29 16:06:33 +0200

    Some work and comments on selector's core method.

--- final_project/GA_Selector.py
+++ final_project/GA_Selector.py
@@ -9,31 +9,36 @@ from ACO_Intensificator import Intensificator
 #Tournament selector
 class Selector_Tournament():
 
-    def do(self,population,pool_size): #car array as population
+    def do(self, population, pool_size): #population = [[car_slots],[demand_slots]] # imagineable as 3D-Array
 
         opponent_amount=2 #more than two "opponents" for the tournament possible
 
-        fitnesses=calc_fitnesses(car_assignments) #not ideal, because maybe not every
-                                            #fitness is interesting, but probably better than calcuting one fitness
-                                            #multiple times
+        fitnesses=calc_fitnesses(population) #not ideal, because maybe not every fitness is interesting, but probably better than calcuting one fitness multiple times
         
-        selected=list()
-        for i in range(pool_size):
+        winners=list() #new population
+        winners.append(list()) #car_slots
+        winners.append(list()) #demand_slots
+
+        #Do as many tournaments as the new population's amount of individuals should be
+        for tournament in range(pool_size):
             opponents=list()
             opponent_fitnesses=list()
             
-            for i2 in range(opponent_amount):
+            #Select opponents for tournament randomly
+            for opponent_number in range(opponent_amount):
 
-                rnd=random.randint(0,len(population)-1)
-                while (rnd in opponents): #in: probably poor performance #where did the do while loop go...
-                    rnd=random.randint(0,len(population)-1)
+                #where did the do while loop go...
+                rnd=random.randint(0,len(population[0])-1)
+                while (rnd in opponents): #in: probably poor performance 
+                    rnd=random.randint(0,len(population[0])-1)
 
                 opponents.append(rnd)
                 opponent_fitnesses.append(fitnesses[rnd])
 
-            selected.append(population[opponents[opponent_fitnesses.index(max(opponent_fitnesses))]])
+            winners[0].append(population[0][opponents[opponent_fitnesses.index(max(opponent_fitnesses))]])
+            winners[1].append(population[1][opponents[opponent_fitnesses.index(max(opponent_fitnesses))]])
 
-        return selected
+        return winners
 
 def summarize(cararray,demandarray): #or are demandarrays included in population?
     sumpop=list()
@@ -45,7 +50,6 @@ def summarize(cararray,demandarray): #or are demandarrays included in population
 
     for index in range(len(cararray)):
         if (index<len(demandarray)):
-
 
             if (cararray[index]!=car):
                 sumpop.append(sumchrom)
