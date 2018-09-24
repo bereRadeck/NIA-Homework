8f0eb7bdc290adb1555dfdf1a76b8fa24dc033a8
basblu <bloombased@gmail.com>
2018-09-24 16:46:19 +0200

    Add files via upload

--- final_project/ACO_Recombiner.py
+++ final_project/ACO_Recombiner.py
@@ -1,36 +1,46 @@
-
-# coding: utf-8
-
-# In[26]:
-
-
 import numpy as np
+import random as rnd
+import copy 
 
 class Recombiner:
     def __init__(self, recomb_chance=0.1):
         self.recomb_chance=recomb_chance
-        
-    #go through all the pairs and recombine them with a certain chance
+       
+    
 
     def do(self,pool):
-        for p in range(len(pool)):
-            for p2 in range(len(pool)):
-                if(np.random.uniform()<self.recomb_chance):
-                    pool[p],pool[p2]=crossover(pool[p],pool[p2])
+        '''go through all the pairs and recombine them with a certain chance'''
         
-    #Here is how you make 1 child from 2 parents:
-    #Step 1: Define two crossover points
-    #Step 2: The section between the two crossover points 
-    #of the first parent is transferred to the first child
-    #    parent 1       4|5 6 7|8
-    #    parent 2       8 6 5 4 7
-    #    child  1       - 5 6 7 -
-    #Step 3: the remaining numbers are inserted in the order in
-    #which they appear in parent 2. 
-    #    parent 2       8 / / 4 /
-    #    child  1       8 5 6 7 4
-    #For the other child you just reverse parent 1 with parent 2
-    
+        #first go through all the items
+        for p1 in range(len(pool)):
+            #then with a certain chance...
+            if(np.random.uniform()<self.recomb_chance):
+                #create crossover point...
+                cr_point1=np.random.choice(range(len(pool[p1])-2))
+                cr_point2=np.random.choice(range(len(pool[p1])-cr_point1-2))+cr_point1+2
+                #search for a fitting partner in the rest of the pool (in random order)...
+                rnge = list(range(len(pool)))
+                rnd.shuffle(rnge)
+                for p2 in rnge:
+                    if not(p2==p1):  
+                        #look whether a potential swapping partner has the same set of
+                        #numbers between those two points (they don't have to have the same order)
+                        
+                        compareto = np.zeros(pool[p1].shape)
+                        compareto[cr_point1:cr_point2]=np.ones(cr_point2-cr_point1)
+                        segment1= pool[p1][cr_point1:cr_point2]
+                        mask=np.isin(pool[p2],segment1)
+                        
+                        #If yes, swap them
+                        if np.array_equal(mask,compareto):
+                            print(pool[p1][cr_point1:cr_point2])
+                            print(pool[p2][cr_point1:cr_point2])
+                            replace_with= copy.deepcopy(pool[p1][cr_point1:cr_point2])
+                            pool[p1][cr_point1:cr_point2]=pool[p2][cr_point1:cr_point2]
+                            pool[p2][cr_point1:cr_point2]=replace_with
+                            break
+        return pool
+ '''   
     def make_child(self,parent1,parent2,cr_point1,cr_point2):
         #take section from parent
         child = np.zeros(parent1.shape)
@@ -55,11 +65,4 @@ class Recombiner:
         child1=self.make_child(parent1,parent2,allele1,allele2)
         child2=self.make_child(parent2,parent1,allele1,allele2)
         return child1,child2
-
-
-# In[29]:
-
-
-recomb = Recombiner()
-print(recomb.do(np.array([1,2,4,6,7,3,5]),np.array([2,1,4,5,3,7,6])))
-
+        '''
