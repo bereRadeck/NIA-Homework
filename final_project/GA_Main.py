6875995feadb649d42cc3004d0d8e1ee25bacb1b
rek <15801406+ljscfo@users.noreply.github.com>
2019-04-11 13:23:03 +0200

    Added Replacer

--- final_project/GA_Main.py
+++ final_project/GA_Main.py
@@ -2,27 +2,30 @@ import GA_Initializer
 import GA_Mutator
 import GA_Recombiner
 import GA_Selector
+import GA_Replacer
 
 class GA:
 
-    def __init__(self,initializer,selector,recombiner,mutator,terminator):
+    def __init__(self, initializer, evaluator, selector, recombiner, mutator, replacer, terminator):
         self.initializer = initializer
+        self.evaluator = evaluator
+        self.selector = selector
         self.recombiner = recombiner
         self.mutator = mutator
-        self.selector = selector
-        #self.terminator = terminator
+        self.replacer = replacer
+        self.terminator = terminator
         #self.pop = self.initializer.initialize()
-
 
     def run(self):
 
         pop = self.initializer.initialize_partially_random()
 
         while not terminator.terminates():
+            pop = self.evaluator.evaluate(pop)
+
+            pop = self.selector.select(mutated_offspring)
             new_offspring = self.recombiner.recombine(pop)
             mutated_offspring = self.mutator.mutate(new_offspring)
-            self.pop = self.selector.select(mutated_offspring)
-
-
+            pop = self.replacer.replace(pop, mutated_offspring)
 
 
---
+++ final_project/GA_Replacer.py
@@ -0,0 +1,9 @@
+
+class Replacer_All:
+
+    def __init__(self):
+        pass
+
+    def replace(pop, offspring):
+        return offspring
+
