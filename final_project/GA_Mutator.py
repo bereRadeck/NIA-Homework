3e0476baf10da15b3a4655a46e8d09f17908693f
basblu <bloombased@gmail.com>
2019-04-11 12:48:31 +0200

    Update GA_Mutator.py

--- final_project/GA_Mutator.py
+++ final_project/GA_Mutator.py
@@ -3,6 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from copy import deepcopy
 
+class Simple_Mutator:
+    
+    def __init__(self,mutation_probability=0.1):
+        self.chance = chance
+        
+    def mutate(self,pool):
+        
+        for p in pool:
+            capacities= p['vehicle_capacities']
+            
+            if rnd.uniform() < self.chance:
+                
+                cr_point1=rnd.choice(range(len(capacities)-1))
+                cr_point2=rnd.choice(range(len(capacities)-cr_point1-1))+cr_point1+1
+                temp=capacities[cr_point1]
+                capacities[cr_point1]=capacities[cr_point2]
+                capacities[cr_point2]=temp
+
 class Mutator:
     #im not sure if i can explain the generell idea well enough in comments. It might be the most informative thing if i make a handwritten / drawen explanation (Mutator/Recombiner Rules Nr2 in clean)
     def do(population,mutation_probability=0.1):
