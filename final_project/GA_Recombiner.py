36e4fc025ca14d592947c36c5a8b4103f8e5e0b9
basblu <bloombased@gmail.com>
2019-04-11 04:34:34 +0200

    Update GA_Recombiner.py
    Changed Bastians recombiner (now renamed simple recombiner)

--- final_project/GA_Recombiner.py
+++ final_project/GA_Recombiner.py
@@ -3,48 +3,35 @@ import random as rnd
 import copy
 
 
-class Ordered_Onecross_Recombiner:
+class Simple_Recombiner:
 
     def __init__(self, combine_probability=0.1):
         self.combine_probability = combine_probability
 
-    def recombine(self, pool):
+    def recombine(self, parents):
 
-        for p in pool:
+        for p in parents:
 
-            if rnd.uniform() < self.combine_probability:
+            
+            new1 = copy.deepcopy(p[0])
+            new2 = copy.deepcopy(p[1])
+            
+            capacities1 = new1['vehicle_capacities']
+            capacities2 = new2['vehicle_capacities']
 
-                p2 = p
-                while p2 == p:
-                    p2 = rnd.choice(pool)
-                capacities1 = p['vehicle_capacities']
-                capacities2 = p2['vehicle_capacities']
-                new1 = copy.deepcopy(capacities1)
-                new2 = copy.deepcopy(capacities2)
+            
+            cr_point1 = rnd.choice(range(len(capacities1) - 1))
+            cr_point2 = rnd.choice(range(len(capacities1) - cr_point1)) + cr_point1 + 1
+            print(cr_point1,cr_point2)
+            slice1 = capacities2[cr_point1:cr_point2]
+            print(len(slice1))    
+            for s in slice1:
+                    
+                del capacities1[capacities1.index(s)]
+            for s in slice1:
+                capacities1.insert(cr_point1, s)
 
-                cr_point1 = rnd.choice(range(len(new1) - 1))
-                cr_point2 = rnd.choice(range(len(new1) - cr_point1)) + cr_point1 + 1
-
-                slice1 = new2[cr_point1:cr_point2]
-                # print(slice1)
-                for s in slice1:
-                    # print(s)
-                    del new1[new1.index(s)]
-                for s in slice1:
-                    new1.insert(cr_point1, s)
-
-                new1_ = copy.deepcopy(capacities1)
-                new2_ = copy.deepcopy(capacities2)
-
-                slice2 = new1_[cr_point1:cr_point2]
-                # print(slice2)
-                for s in slice2:
-                    del (new2_[new2_.index(s)])
-                for s in slice2:
-                    new2_.insert(cr_point1, s)
-
-                p['vehicle_capacities'] = new1
-                p2['vehicle_capacities'] = new2_
+            return new1
 
 
 class Ordered_Recombiner:
