a3b64c3cb5be94160b90d8635b11d7cc25cd367d
basblu <bloombased@gmail.com>
2019-04-11 13:08:51 +0200

    Update Test_GA_Simple_Rec.py

--- final_project/test/Test_GA_Simple_Rec.py
+++ final_project/test/Test_GA_Simple_Rec.py
@@ -1,10 +1,11 @@
 from final_project.Recombiner import SimpleRecombiner
+from copy import deepcopy
 
 testlist=list(range(14))
 testlist2=list(range(14))
 testlist2.reverse()
 parents = []
-parents = [({'vehicle_capacities':testlist,'customer_demands':testlist,'capacities_list':[5,2,5,3,2,2,2,4,1,3]},
-              {'vehicle_capacities':testlist2,'customer_demands':testlist2,'capacities_list':[5,2,5,3,2,2,2,4,1,3]})]
+parents = [({'vehicle_capacities':testlist,'customer_demands':deepcopy(testlist),'capacities_list':[5,2,5,3,2,2,2,4,1,3]},
+              {'vehicle_capacities':testlist2,'customer_demands':deepcopy(testlist2),'capacities_list':[5,2,5,3,2,2,2,4,1,3]})]
 SR= Simple_Recombiner()
 print(SR.recombine(parents))
